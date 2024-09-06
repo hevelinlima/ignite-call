8bb9a0d869d0e09ca193ff07987e255e0d3ebc4c
hevelinlima <hevelin25@gmail.com>
2024-09-05 23:03:56 -0300

    feat: add Prisma and creating user model

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -6,6 +6,7 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { api } from "@/src/lib/axios";
 
 const registerFormSchema = z.object({
   username: z.string()
@@ -38,7 +39,15 @@ export default function Register() {
   }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        username: data.username, 
+        name: data.name
+      })
+
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return(
